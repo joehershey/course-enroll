457decb0aac0110342f25ca9e78ca051844f2fd5
joehershey <56175567+joehershey@users.noreply.github.com>
2020-10-21 00:48:15 -0500

    Update Rating.js

--- src/Rating.js
+++ src/Rating.js
@@ -13,30 +13,56 @@ class Rating extends React.Component {
   }
   render() {
     return (
-      <Form.Group controlId="formRating">
-        <Form.Control
-          as="select"
-          ref={this.rating}
-          onChange={() => this.setRecs()}
-        >
-          {this.getRatingOptions()}
-        </Form.Control>
-      </Form.Group>
+      <div>
+        <Form.Group controlId="formRating">
+          <Form.Control
+            as="select"
+            ref={this.rating}
+            onChange={() => this.setRecs()}
+          >
+            {this.getRatingOptions()}
+          </Form.Control>
+        </Form.Group>
+      </div>
     );
   }
   getRatingOptions() {
     let ratingOptions = [];
+    let one = "\u2605";
+    let two = one + one;
+    let three = two + one;
+    let four = three + one;
+    let five = four + one;
     ratingOptions.push(<option key={0}>No Rating</option>);
-    ratingOptions.push(<option key={1}>1</option>);
-    ratingOptions.push(<option key={2}>2</option>);
-    ratingOptions.push(<option key={3}>3</option>);
-    ratingOptions.push(<option key={4}>4</option>);
-    ratingOptions.push(<option key={5}>5</option>);
+    ratingOptions.push(<option key={1}>{one}</option>);
+    ratingOptions.push(<option key={2}>{two}</option>);
+    ratingOptions.push(<option key={3}>{three}</option>);
+    ratingOptions.push(<option key={4}>{four}</option>);
+    ratingOptions.push(<option key={5}>{five}</option>);
     return ratingOptions;
   }
   setRecs() {
     if (this.rating.current != null) {
-      this.props.rating(this.rating.current.value, this.props.completed);
+      console.log(this.rating.current.value);
+      if (this.rating.current.value !== "No Rating") {
+        if (this.rating.current.value.length === 1) {
+          this.props.rating(1, this.props.completed);
+        }
+        if (this.rating.current.value.length === 2) {
+          this.props.rating(2, this.props.completed);
+        }
+        if (this.rating.current.value.length === 3) {
+          this.props.rating(3, this.props.completed);
+        }
+        if (this.rating.current.value.length === 4) {
+          this.props.rating(4, this.props.completed);
+        }
+        if (this.rating.current.value.length === 5) {
+          this.props.rating(5, this.props.completed);
+        }
+      } else {
+        this.props.rating(this.rating.current.value, this.props.completed);
+      }
     }
   }
 }
